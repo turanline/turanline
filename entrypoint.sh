7b252b16fbad278eb374e5ce358cbf2d61302c24
Кузнецов Илья <KuznetsovIlyaDM@yandex.ru>
2024-05-05 02:23:34 +0300

    delete extra entrypoint file

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-poetry run python3 manage.py collectstatic --noinput --force
-poetry run python3 manage.py makemigrations --noinput
-poetry run python3 manage.py migrate --noinput
-poetry run gunicorn --bind 0.0.0.0:8000 mssite.wsgi:application --log-level info
